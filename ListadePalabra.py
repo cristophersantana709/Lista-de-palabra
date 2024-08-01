b8d5168428239a361f26c20b15835d7815a942d1
cristopher santana <cristopher.santana@tecnologicoliceocristiano.edu.ec>
2024-08-01 17:46:29 -0500

    Lista de palabra

---
+++ ListadePalabra.py
@@ -0,0 +1,6 @@
+# Objetivo: Practicar la iteración sobre listas y tuplas.
+# Instrucciones: Dada una lista de palabras ['python', 'es', 'genial'], usa un ciclo for para imprimir cada palabra en mayúsculas.
+
+palabra = ['python', 'es', 'genial']
+palabra1 = [letra.upper() for letra in palabra]
+print(palabra1)
